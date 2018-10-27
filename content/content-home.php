a023a38431846096e9c6629c952b7d3e6e4f44b2
lucaiustin96 <lucaiustin96@gmail.com>
2018-10-27 19:34:03 +0300

    Am facut cateva modificari la menu, am adaugat o functie pentru drop-down, am modificat putin structura site-ului, am facut niste modificari la layout-ul de pe mobil si tableta.

--- content/content-home.php
+++ content/content-home.php
@@ -1,5 +1,7 @@
 <article class = "article-wrapper">
-	<section class= "section-description">
+	<section class= "section-info">
+		<div class = "section-info-title">#METOO</div>
+		<div class = "section-info-description">Just another website description!</div>
 	</section>
 	<section class= "section-content">
 		Lorem ipsum dolor sit amet, minim pericula definiebas eum te, adhuc ridens in est. Ea has prima dolor. At omnium fabulas epicurei eum. An qui dicat eripuit, ne usu debet saperet nominavi, quidam referrentur te quo. Natum legere assentior cu mea, sed ad aeque accusata intellegebat.
--- header.php
+++ header.php
@@ -1,8 +1,10 @@
 <header class= "header-wrapper">
 	<div class= "header">
-		<div class = "menu-icon"></div>
+		<div class = "menu-icon" onclick="displayMenu()">
+			<span class="icon-menu"></span>
+		</div>
 		<div class= "logo">Metoo</div>
-		<nav class= "nav">
+		<nav id = "nav">
 			<ul>
 				<li><a href= "home">Home</a></li>
 				<li><a href= "home">Test1</a></li>
