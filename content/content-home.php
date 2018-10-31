c0917640b346b56fc47b033cfcee4fed9136543a
lucaiustin96 <lucaiustin96@gmail.com>
2018-10-31 08:19:49 +0200

    Mici modificari

--- content/content-home.php
+++ content/content-home.php
@@ -1,7 +1,7 @@
 <article>
 	<div class = "article-wrapper">
 		<section>
-			<div class= "section-info"><
+			<div class= "section-info">
 				<div class = "section-info-title">#METOO</div>
 				<div class = "section-info-description">Just another website description!</div>
 			</div>
@@ -19,6 +19,6 @@
 				<br><br>
 				Lorem ipsum dolor sit amet, minim pericula definiebas eum te, adhuc ridens in est. Ea has prima dolor. At omnium fabulas epicurei eum. An qui dicat eripuit, ne usu debet saperet nominavi, quidam referrentur te quo. Natum legere assentior cu mea, sed ad aeque accusata intellegebat.Lorem ipsum dolor sit amet, minim pericula definiebas eum te, adhuc ridens in est. Ea has prima dolor. At omnium fabulas epicurei eum. An qui dicat eripuit, ne usu debet saperet nominavi, quidam referrentur te quo. Natum legere assentior cu mea, sed ad aeque accusata intellegebat.
 			</div>
-	</section>
+		</section>
 	</div>
 </article>
